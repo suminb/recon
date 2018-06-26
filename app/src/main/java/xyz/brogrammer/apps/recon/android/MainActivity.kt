c87c672c5e8ca989b08dc57ee7795c15ddba29a5
Sumin Byeon <suminb@gmail.com>
2018-06-27 03:53:06 +0900

    Getting GPS locations

--- app/src/main/java/xyz/brogrammer/apps/recon/android/MainActivity.kt
+++ app/src/main/java/xyz/brogrammer/apps/recon/android/MainActivity.kt
@@ -6,25 +6,29 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
 import xyz.brogrammer.apps.android_recon.android.R
 
 class MainActivity : AppCompatActivity() {
 
     private val PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION = 1001
+    private val PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION = 1002
 
     var cache = HashMap<String, Record>()
+    var currentLocation: Location? = null
     lateinit var wifiManager: WifiManager
+    lateinit var locationManager: LocationManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +41,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         wifiManager = this.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -66,23 +71,46 @@ class MainActivity : AppCompatActivity() {
         } else {
             startScanning()
         }
+
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION)
+        } else {
+            startLocationService()
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         startScanning()
+        startLocationService()
     }
 
     private val broadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
-            Log.d("TEST", "%s".format(wifiManager.scanResults))
+            // Log.d("TEST", "%s".format(wifiManager.scanResults))
             for (result in wifiManager.scanResults) {
                 val mac = result.BSSID
                 cache[mac] = Record(mac, result.SSID, result.level, result.frequency,
                         result.channelWidth, result.timestamp, result.capabilities)
-                Log.d("TEST", cache[mac].toString())
+                // Log.d("TEST", cache[mac].toString())
             }
-            Log.d("TEST", "# of records = %d".format(cache.size))
+            // Log.d("TEST", "# of records = %d".format(cache.size))
+        }
+    }
+
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            Log.d("TEST", "%f, %f".format(location.longitude, location.latitude));
+            currentLocation = location
+        }
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+            Log.d("TEST", "onStatusChanged")
+        }
+        override fun onProviderEnabled(provider: String) {
+            Log.d("TEST", "onProviderEnabled")
+        }
+        override fun onProviderDisabled(provider: String) {
+            Log.d("TEST", "onProviderDisabled")
         }
     }
 
@@ -100,5 +128,13 @@ class MainActivity : AppCompatActivity() {
     private fun stopScanning() {
         unregisterReceiver(broadcastReceiver)
         Log.d("TEST", "stopScanning()")
+    }
+
+    private fun startLocationService() {
+        try {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 1f, locationListener);
+        } catch(ex: SecurityException) {
+            Log.e("ERROR", ex.localizedMessage)
+        }
     }
 }
